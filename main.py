98e4995a6d9ee6fa5a5687742b23787cfe83fddb
Ken-kichi <PoTi15673cmgpjwp@yahoo.co.jp>
2025-05-15 22:00:16 +0900

    pathを追加

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes import auth_routes, task_routes,user_routes
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+frontend_path = os.environ["FRONTEND_PATH"] | "http://localhost:3000"
 
 app = FastAPI(
     title="Task App",
@@ -13,7 +19,7 @@ app.include_router(task_routes.router)
 app.include_router(user_routes.router)
 
 origins = [
-    "http://localhost:3000",
+    frontend_path,
     "http://127.0.0.1:3000",
 ]
 
